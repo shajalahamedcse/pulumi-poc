6c50ac3c212367bc1dafdb7dc9c57a85148fd905
root <shajalahamedcse@gmail.com>
2024-06-29 16:17:13 +0000

    Added igw

--- __main__.py
+++ __main__.py
@@ -17,3 +17,58 @@ public_subnet = aws.ec2.Subnet("public-subnet",
     )
 
 igw = aws.ec2.InternetGateway("igw",vpc_id=vpc.id)
+
+# Create Route Table
+route_table = aws.ec2.RouteTable("route-table",
+    vpc_id=vpc.id,
+    routes=[{
+        "cidr_block": "0.0.0.0/0",
+        "gateway_id": igw.id,
+    }],
+)
+
+# Associate Route Table with Public Subnet
+rt_assoc_public = aws.ec2.RouteTableAssociation("rt-assoc-public",
+    subnet_id=public_subnet.id,
+    route_table_id=route_table.id,
+)
+
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("web-secgrp",
+    description='Enable SSH and K3s access',
+    vpc_id=vpc.id,
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 6443,
+            "to_port": 6443,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+    ],
+    egress=[{
+        "protocol": "-1",
+        "from_port": 0,
+        "to_port": 0,
+        "cidr_blocks": ["0.0.0.0/0"],
+    }],
+)
+
+
+ami_id = "ami-008c09a18ce321b3c"  # Replace with a valid AMI ID for your region
+instance_type = "t3.small"
+
+master_node = aws.ec2.Instance("master-node",
+    instance_type=instance_type,
+    ami=ami_id,
+    subnet_id=public_subnet.id,
+    key_name=key_pair.key_name,
+    vpc_security_group_ids=[security_group.id],
+    tags={
+        "Name": "master-node"
+    })
