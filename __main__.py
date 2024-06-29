ee0b2753e05002c7dc35a067dd8d8686e059deaa
root <shajalahamedcse@gmail.com>
2024-06-29 16:09:46 +0000

    Added subnet

--- __main__.py
+++ __main__.py
@@ -8,3 +8,10 @@ vpc = aws.ec2.Vpc("my-vpc",
     enable_dns_hostnames=True,
     enable_dns_support=True,
     )
+
+public_subnet = aws.ec2.Subnet("public-subnet",
+    vpc_id = vpc.id,
+    cidr_block = "10.10.1.0/24",
+    map_public_ip_on_launch=True,
+    availability_zone='ap-southeast-1a',
+    )
